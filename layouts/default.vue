85c1b178408e49f03aa962d300e86228804e071a
alisalad <Ali>
2020-06-13 01:29:31 +1000

    all files

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AppHeader />
+    <nuxt />
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader'
+import AppFooter from '../components/AppFooter'
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+
